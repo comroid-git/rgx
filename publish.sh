da79864b2ecb912a4ba812c5c8adbb1c7a798d94
burdoto <kaleidox@comroid.org>
2023-05-08 15:09:56 +0200

    Revert "do not commit SRCINFO"
    This reverts commit adc519d4db6124f4478ccfc26d354af19debee45.

--- publish.sh
+++ publish.sh
@@ -12,6 +12,7 @@ makepkg -f --noconfirm
 
 # update SRCINFO
 makepkg --printsrcinfo > .SRCINFO
+(git add .SRCINFO && git commit -m "SRCINFO" && git push) || (echo "Failed to commit .SRCINFO" >&2 | return)
 
 # push to aur
 if [ -z "$(git remote | grep aur)" ]; then
