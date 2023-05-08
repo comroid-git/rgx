9cd6562d2a4585ca7ba03acb9d52cc45f557d726
burdoto <kaleidox@comroid.org>
2023-05-08 14:54:43 +0200

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -3,5 +3,6 @@
 set -e
 
 ./clean.sh
+dotnet publish -c Test --use-current-runtime || echo "Tests failed" >&2
 dotnet publish -c Debug --use-current-runtime || echo "Build failed" >&2
-bin/Release/net7.0/linux-x64/publish/rgx || echo "Execute failed" >&2
+bin/Debug/net7.0/linux-x64/publish/rgx --help || echo "Execute failed" >&2
