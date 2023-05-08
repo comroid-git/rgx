1a41f7f26c4ca1869ff142c028bd5df59d473d37
burdoto <kaleidox@comroid.org>
2023-05-08 15:16:38 +0200

    reorder

--- publish.sh
+++ publish.sh
@@ -7,12 +7,12 @@ set -e # exit on error
 # run tests first
 dotnet test -c Test
 
-# build the executable
-makepkg -f --noconfirm
-
 # update SRCINFO
 makepkg --printsrcinfo > .SRCINFO
 (git add .SRCINFO && git commit -m "SRCINFO" && git push) || (echo "Failed to commit .SRCINFO" >&2 | return)
+
+# build the executable
+makepkg -f --noconfirm
 
 # push to aur
 if [ -z "$(git remote | grep aur)" ]; then
